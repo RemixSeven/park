30edcdfc3f5b6fcb6e4b71d07ba32fbf23f79499
yuanmingwang <wangyuanming7@gmail.com>
2018-05-13 00:00:11 +0800

    fix bug+新增评论、钱包相关接口

---
+++ src/main/java/com/ymwang/park/dto/Charge/QueryChargeRequest.java
@@ -0,0 +1,12 @@
+package com.ymwang.park.dto.Charge;
+
+import lombok.Data;
+
+/**
+ * @Author: wym
+ * @Date: 2018/5/11
+ */
+@Data
+public class QueryChargeRequest {
+    private String userId;
+}
